769e30a64702d43aa1053f5cf854a3437a0145bd
haychs13 <hayderasayyid@gmail.com>
2026-03-17 04:50:51 +0000

    Delete __init__.py
    accidentally uploaded the file in the wrong place XD

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .brute_force import detect_brute_force
-from .scan import detect_scan
-from .unusual_ip import detect_unusual_ip
-
-__all__ = ["detect_brute_force", "detect_scan", "detect_unusual_ip"]
